415861a79cdb95467d72a84ecda26739608d940d
dieg0cr98 <dieg0cr98@gmail.com>
2019-10-07 00:24:34 -0600

    testing drag and drop

--- Proyecto/ProyectoIntegrador/Controllers/HomeController.cs
+++ Proyecto/ProyectoIntegrador/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProyectoIntegrador.BaseDatos;
 
 namespace ProyectoIntegrador.Controllers
 {
+
+
+
     public class HomeController : Controller
     {
+
+
+        private Gr03Proy2Entities3 db = new Gr03Proy2Entities3();
         public ActionResult Index()
         {
             return View();
@@ -15,6 +22,8 @@ namespace ProyectoIntegrador.Controllers
 
         public ActionResult About()
         {
+
+            ViewBag.empleados = db.Empleado.ToList();
             ViewBag.Message = "Your application description page.";
 
             return View();
