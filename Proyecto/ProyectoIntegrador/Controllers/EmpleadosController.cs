490cda70b1503390bdab67b4ee1bf9ae60f44ddd
Alexs06 <49921375+Alexs06@users.noreply.github.com>
2019-10-31 09:13:13 -0600

    lib included

--- Proyecto/ProyectoIntegrador/Controllers/EmpleadosController.cs
+++ Proyecto/ProyectoIntegrador/Controllers/EmpleadosController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using ProyectoIntegrador.BaseDatos;
 using ProyectoIntegrador.Models;
+using System.Data.SqlClient;
 
 namespace ProyectoIntegrador.Controllers
 {
