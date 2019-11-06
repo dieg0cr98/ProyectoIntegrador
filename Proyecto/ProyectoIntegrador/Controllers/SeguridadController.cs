35d1c1102a62f8b56df8d96845ffb996049adcda
kelarao7 <32776061+kelarao7@users.noreply.github.com>
2019-11-05 20:46:18 -0600

    Seguridad Empleados

--- Proyecto/ProyectoIntegrador/Controllers/SeguridadController.cs
+++ Proyecto/ProyectoIntegrador/Controllers/SeguridadController.cs
@@ -304,6 +304,58 @@ namespace ProyectoIntegrador.Controllers
          /*Eliminar*/                    {1,3,3,3}
          };
         //---------------------------------------------------------------------------------------------------------------------------//
+/* 1 Pueder hacer la accion para todos los empleados(CRUD)
+     * 2 Solo a los empleados que trabajan en un proyecto donde el es el lider
+     * 3 No puede hacer la accion 
+    */
+        private int[,] tablaSeguridadEmpleados = new int[,] {
+                       /*Soporte/Calidad , Lider , Tester , Cliente*/
+         /*Consultar*/                   {1,2,2,3},
+         /*Agregar*/                     {1,3,3,3},
+         /*Editar*/                      {1,3,3,3},
+         /*Eliminar*/                    {1,3,3,3}
+         };
+        //---------------------------------------------------------------------------------------------------------------------------//
+ /*Metodo para acceder a los permisos del usuario en la vista de consultarClientes
+     * Retorna un Tuple<int,string,int,int,int>, con los valores:
+     *              rol (0 Soporte/Calidad , 1 Lider , 2 Tester , 3 Cliente)
+     *              permisoConsultar (valor recuperado en la tabla de tablaSeguridadClientes)
+     *              cedulaUsuario
+     *              permisoEditar (valor recuperado en la tabla de tablaSeguridadClientes)
+     *              permisoAgregar (valor recuperado en la tabla de tablaSeguridadClientes)
+     *              permisoBorrar (valor recuperado en la tabla de tablaSeguridadClientes)
+    */
+        public Tuple<int, string, int, int, int, int> EmpleadoConsultar(System.Security.Principal.IPrincipal user)
+        {
+
+            int permisoConsultar = 3; //Por defecto no puede consultar
+            string cedulaUsuario = "";
+            int permisoEditar = 3;   //Por defecto no puede editar
+            int permisoAgregar = 3;   //Por defecto no puede editar
+            int permisoBorrar = 3;   //Por defecto no puede editar
+
+
+            //Obtiene el rol del usuario
+            int rol = GetRoleUsuario(user);
+
+            if (rol >= 0)// Si el usuario tiene un rol asignado
+            {
+                //Obtine los permisos de la tabla de Seguridad
+                permisoConsultar = tablaSeguridadEmpleados[0, rol];
+                permisoAgregar = tablaSeguridadEmpleados[1, rol]; ;
+                permisoEditar = tablaSeguridadEmpleados[2, rol]; ;
+                permisoBorrar = tablaSeguridadEmpleados[3, rol]; ;
+
+                if (permisoConsultar == 2)//Solo puede ver los proyectos a los cuales pertenece
+                {
+                    //Para que el controlador haga un filtro se ocupa pasar la cedula del usuario
+                    cedulaUsuario = IdUsuario(user);
+                }
+
+            }
+
+            return Tuple.Create(rol, cedulaUsuario, permisoConsultar, permisoEditar, permisoAgregar, permisoBorrar);
+        }
 
         /*Metodo para acceder a los permisos del usuario en la vista de consultarClientes
          * Retorna un Tuple<int,string,int,int,int>, con los valores:
