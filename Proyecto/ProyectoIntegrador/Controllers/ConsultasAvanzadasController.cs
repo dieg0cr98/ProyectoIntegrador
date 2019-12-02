81ca5ed7805d272ceedc1584bbda0ae0930b1889
kelarao7 <32776061+kelarao7@users.noreply.github.com>
2019-12-02 12:25:18 -0600

    Consulta 8

--- Proyecto/ProyectoIntegrador/Controllers/ConsultasAvanzadasController.cs
+++ Proyecto/ProyectoIntegrador/Controllers/ConsultasAvanzadasController.cs
@@ -269,7 +269,42 @@ namespace ProyectoIntegrador.Controllers
             var json = JsonConvert.SerializeObject(t);
             return Json(json, JsonRequestBehavior.AllowGet);
         }
+ /*
+        * Efecto: Obtiene todos los valores requeridos en la cuarta consulta avanzada  y los devuelve en un Json.
+        * Requiere: int rol. 0 = Calidad/Soporte, 1= Lider, 2 = Tester, 3 = Cliente
+        *           int permiso. 1= total , 2 = parcial , 3 = Ninguno
+        *           string idUsuario. Identificacion del usuario
+        * Modifica: 
+        * Retorna:  JsonResult con los proyectos en los que participa un usuario
+        */
+        public JsonResult consulta8(int rol, int permiso, string idUsuario)
+        {
+            var t = db.USP_ObtenerProyectosActivosTerminadosUsuario(permiso, rol, idUsuario);
 
+            var json = JsonConvert.SerializeObject(t, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+
+        /*
+        * Efecto: Obtiene todos los valores requeridos para cargar la tabla de testers en la cuarta consulta avanzada  y los devuelve en un Json.
+        * Requiere: int id. Identificador del proyecto
+        * Modifica: 
+        * Retorna:  JsonResult con los tester asignados a un proyecto especifico
+        */
+        public JsonResult consulta8LoadTablePruebas(int id)
+        {
+
+            var t = db.USP_ObtenerPruebasRequerimient(id);
+            var json = JsonConvert.SerializeObject(t);
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+
+        }
  /*
         * Efecto: Obtiene todos los valores requeridos en la cuarta consulta avanzada  y los devuelve en un Json.
         * Requiere: int rol. 0 = Calidad/Soporte, 1= Lider, 2 = Tester, 3 = Cliente
